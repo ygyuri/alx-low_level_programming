b745d3e55ce536d957a8a0e4417a0c6f09adc945
Ygyuri <gideonyuri15@gmail.com>
2023-02-16 02:10:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,9 @@
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
 char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";

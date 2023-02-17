bc8ecd46e5188c2b14bd7070e8220f507295dee8
Ygyuri <gideonyuri15@gmail.com>
2023-02-17 11:23:12 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,27 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
-int main() {
-  // Initialize random seed
-  std::srand(std::time(nullptr));
-  
-  // Generate random number between -100 and 100
-  int n = std::rand() % 201 - 100;
-  
-  // Print the random number
-  std::cout << "The number " << n << " ";
-  
-  // Check if the number is positive, negative, or zero
-  if (n > 0) {
-    std::cout << "is positive";
-  } else if (n < 0) {
-    std::cout << "is negative";
-  } else {
-    std::cout << "is zero";
-  }
-  
-  // Print a new line
-  std::cout << std::endl;
-  
-  return 0;
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+return (0);
 }
-

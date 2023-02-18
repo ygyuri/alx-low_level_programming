2fdfc14fde84250a9bc2ae7b704f5fee1ec299c4
Ygyuri <gideonyuri15@gmail.com>
2023-02-17 18:01:35 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+int main(void)
+{
+    int i, j, k, l;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            for (k = i; k <= 9; k++)
+            {
+                if (k == i)
+                {
+                    l = j + 1;
+                }
+                else
+                {
+                    l = 0;
+                }
+
+                for (; l <= 9; l++)
+                {
+                    putchar(i + '0');
+                    putchar(j + '0');
+                    putchar(' ');
+                    putchar(k + '0');
+                    putchar(l + '0');
+
+                    if (i == 9 && j == 8 && k == 9 && l == 9)
+                    {
+                        putchar('\n');
+                    }
+                    else
+                    {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
+
+    return (0);
+}
+
